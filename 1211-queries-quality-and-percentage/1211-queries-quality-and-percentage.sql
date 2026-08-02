2943201b4c0198cca624a16e3a1f06e5c642750a
kawser hamim <mdkawserhamim91824@gmail.com>
2026-08-02 13:21:05 +0600

    Time: 238 ms (45.31%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,13 @@
+SELECT 
+    query_name,
+    ROUND(AVG(rating * 1.0 / position), 2) AS quality,
+    ROUND(
+        AVG(CASE WHEN rating < 3 THEN 1.0 ELSE 0.0 END) * 100, 
+        2
+    ) AS poor_query_percentage
+FROM 
+    Queries
+WHERE 
+    query_name IS NOT NULL
+GROUP BY 
+    query_name;

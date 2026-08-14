ecdaadaae495cf69663b298d59e3b08d718ca494
kawser hamim <mdkawserhamim91824@gmail.com>
2026-08-14 10:59:25 +0600

    Time: 316 ms (50.76%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,2 +1,3 @@
-select  name , population , area from world
-where area >= 3000000 or population >= 25000000
+-- Write your PostgreSQL query statement below
+select name , population , area from World
+where population >= 25000000 or area >= 3000000 ;

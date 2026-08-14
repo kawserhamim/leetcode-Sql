be1f182e18cf796076bdc5f8a21684e95ef19b45
kawser hamim <mdkawserhamim91824@gmail.com>
2026-08-14 10:28:39 +0600

    Time: 226 ms (73.49%), Space: 0B (100%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,3 +1,4 @@
 -- Write your PostgreSQL query statement below
-select product_id from Products 
-where low_fats = 'Y' and recyclable = 'Y'
+-- select * from products ;
+select product_id from  products 
+where low_fats = 'Y' and recyclable  = 'Y' ;

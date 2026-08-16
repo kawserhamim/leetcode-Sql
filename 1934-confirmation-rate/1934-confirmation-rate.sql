85bb041757979a0c1edfe6a07b880608e65ea3c4
kawser hamim <mdkawserhamim91824@gmail.com>
2026-08-16 19:51:38 +0600

    Time: 320 ms (12.02%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,16 @@
+SELECT 
+    s.user_id,
+    COALESCE(
+        ROUND(
+            COUNT(CASE WHEN c.action = 'confirmed' THEN 1 END) * 1.0 
+            / NULLIF(COUNT(c.action), 0), 
+            2
+        ), 
+        0
+    ) AS confirmation_rate
+FROM 
+    Signups s
+LEFT JOIN 
+    Confirmations c ON s.user_id = c.user_id
+GROUP BY 
+    s.user_id;
